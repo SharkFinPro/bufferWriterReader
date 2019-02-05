39fe8c8ca50e353db13a149b21dbb4f24f328a80
SharkFin <sharkfinpro@hotmail.com>
2019-02-04 17:54:07 -0800

    made this thing actually work lol
    removed comments as well ^-^

--- browser.js
+++ browser.js
@@ -1,135 +1,145 @@
-(function() { // Run everything in a function that auto runs to add a level fo security
+(function() {
     class Writer {
         constructor(size) {
-            this.index = 0; // Set index to start of buffer
-            this.buffer = new DataView(new ArrayBuffer(size)); // Set buffer as passed buffer argument
+            this.index = 0;
+            this.buffer = new DataView(new ArrayBuffer(size));
         }
 
         toBuffer() {
-            this.index = 0; // Set index back to 0
-            return this.buffer.buffer; // Return the buffer
+            this.index = 0;
+            return this.buffer.buffer;
         }
 
         writeInt8(n) {
-            this.buffer.setInt8(this.index++, n); // Write Int8 Data & Move index 1 byte
+            this.buffer.setInt8(this.index++, n);
             return this;
         }
 
         writeUInt8(n) {
-            this.buffer.setUInt8(this.index++, n); // Write UInt8 Data & Move index 1 byte
+            this.buffer.setUInt8(this.index++, n);
             return this;
         }
 
         writeInt16(n, le) {
-            this.buffer.setInt16(this.index += 2, n, le)); // Write Int16 Data & Move index 2 bytes
+            this.buffer.setInt16(this.index, n, le);
+            this.index += 2;
             return this;
         }
 
         writeUInt16(n, le) {
-            this.buffer.setUInt16(this.index += 2, n, le)); // Write UInt16 Data & Move index 2 bytes
+            this.buffer.setUInt16(this.index, n, le);
+            this.index += 2;
             return this;
         }
 
         writeInt32(n, le) {
-            this.buffer.setInt32(this.index += 4, n)); // Write Int32 Data & Move index 4 bytes
+            this.buffer.setInt32(this.index, n);
+            this.index += 4;
             return this;
         }
 
         writeUInt32(n, le) {
-            this.buffer.setUInt32(this.index += 4, n)); // Write UInt32 Data & Move index 4 bytes
+            this.buffer.setUInt32(this.index, n);
+            this.index += 4;
             return this;
         }
 
         writeFloat32(n, le) {
-            this.buffer.writeFloat(this.index += 4, n)); // Write Float Data & Move index 2 bytes
+            this.buffer.writeFloat(this.index, n);
+            this.index += 4;
             return this;
         }
 
         writeString8(n) {
-            if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-            for (var i in n) this.writeUInt8(n.charCodeAt(i)); // Write a UInt8 of each character's char code.
-            return this.writeUInt8(0); // Write a 0 to signify end of string.
+            if (typeof n !== 'string') return console.error('Data must be a string.');
+            for (var i in n) this.writeUInt8(n.charCodeAt(i));
+            return this.writeUInt8(0);
         }
 
         writeString16(n, le) {
-            if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-            for (var i in n) this.writeUInt16(n.charCodeAt(i), le); // Write a UInt16 of each character's char code.
-            return this.writeUInt16(0); // Write a 0 to signify end of string.
+            if (typeof n !== 'string') return console.error('Data must be a string.');
+            for (var i in n) this.writeUInt16(n.charCodeAt(i), le);
+            return this.writeUInt16(0);
         }
 
         writeString32(n, le) {
-            if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-            for (var i in n) this.writeUInt32(n.charCodeAt(i), le); // Write a UInt32 of each character's char code.
-            return this.writeUInt32(0); // Write a 0 to signify end of string.
+            if (typeof n !== 'string') return console.error('Data must be a string.');
+            for (var i in n) this.writeUInt32(n.charCodeAt(i), le);
+            return this.writeUInt32(0);
         }
     }
 
     class Reader {
         constructor(buffer) {
-            this.index = 0; // Set index to start of buffer
-            this.buffer = buffer; // Set buffer as passed buffer argument
+            this.index = 0;
+            this.buffer = new DataView(buffer);
         }
 
         readInt8() {
-            return this.buffer.readInt8(this.index++); // Return read Int8 data & Move index 1 byte
+            return this.buffer.getInt8(this.index++);
         }
 
         readUInt8() {
-            return this.buffer.readUInt8(this.index++); // Return read UInt8 data & Move index 1 byte
+            return this.buffer.getUint8(this.index++)
         }
 
         readInt16(le) {
-            this.index += 2; // Move index 2 bytes
-            return le ? this.buffer.readInt16(this.index - 2) : this.buffer.readInt16(this.index - 2)); // Return read Int16 data
+            this.index += 2;
+            return this.buffer.getInt16(this.index - 2, le || false);
         }
 
         readUInt16(le) {
-            this.index += 2; // Move index 2 bytes
-            return le ? this.buffer.readUInt16(this.index - 2) : this.buffer.readUInt16(this.index - 2)); // Return read UInt16 data
+            this.index += 2;
+            return this.buffer.getUint16(this.index - 2, le || false);
         }
 
         readInt32(le) {
-            this.index += 4; // Move index 4 bytes
-            return le ? this.buffer.readInt32(this.index - 4) : this.buffer.readInt32(this.index - 4)); // Return read Int32 data
+            this.index += 4;
+            return this.buffer.getInt32(this.index - 4, le || false)
         }
 
         readUInt32(le) {
-            this.index += 4; // Move index 4 bytes
-            return le ? this.buffer.readUInt32(this.index - 4) : this.buffer.readUInt32(this.index - 4)); // Return read UInt32 data
+            this.index += 4;
+            return this.buffer.getUint32(this.index - 4, le || false);
         }
 
         readFloat32(le) {
-            this.index += 4; // Move index 4 bytes
-            return le ? this.buffer.readFloat(this.index - 4) : this.buffer.readFloat(this.index - 4)); // Return read Float data
+            this.index += 4;
+            return this.buffer.getFloat32(this.index - 4, le || false);
+        }
+
+        readFloat64(le) {
+            this.index += 8;
+            return this.buffer.getFloat64(this.index - 8, le || false);
         }
 
         readString8() {
-            let data = ''; // Set data as a string
-            while (true) { // Run following code until it returns itself
-                let char = this.readUInt8(); // Read char code as UInt8
-                if (char === 0) return data; // Return the string when the character is 0
-                data += String.fromCharCode(char); // Add the character to the data string
+            let data = '';
+            while (true) {
+                let char = this.readUInt8();
+                if (char === 0) return data;
+                data += String.fromCharCode(char);
             }
         }
 
         readString16(le) {
-            let data = ''; // Set data as a string
-            while (true) { // Run following code until it returns itself
-                let char = this.readUInt16(le); // Read char code as UInt16
-                if (char === 0) return data; // Return the string when the char code is 0
-                data += String.fromCharCode(char); // Add the character to the data string
+            let data = '';
+            while (true) {
+                let char = this.readUInt16(le);
+                if (char === 0) return data;
+                data += String.fromCharCode(char);
             }
         }
 
         readString32(le) {
-            let data = ''; // Set data as a string
-            while (true) { // Run following code until it returns itself
-                let char = this.readUInt32(le); // Read char code as UInt32
-                if (char == 0) return data; // Return the string when the char code is 0
-                data += String.fromCharCode(char); // Add the character to the data string
+            let data = '';
+            while (true) {
+                let char = this.readUInt32(le);
+                if (char == 0) return data;
+                data += String.fromCharCode(char);
             }
         }
     }
 
-    window.Buffer = { Writer, Reader }; // Set Writer & Reader as properties in window.Buffer
+    window.Buffer = { Writer, Reader };
 })();
--- node.js
+++ node.js
@@ -1,133 +1,138 @@
 class Writer {
     constructor(size) {
-        this.index = 0; // Set index to start of buffer
-        this.buffer = Buffer.alloc(size); // Set buffer as passed buffer argument
+        this.index = 0;
+        this.buffer = Buffer.alloc(size);
     }
 
     toBuffer() {
-        this.index = 0; // Set index back to 0
-        return this.buffer; // Return the buffer
+        this.index = 0;
+        return this.buffer;
     }
 
     writeInt8(n) {
-        this.buffer.writeInt8(n, this.index++); // Write Int8 Data & Move index 1 byte
+        this.buffer.writeInt8(n, this.index++);
         return this;
     }
 
     writeUInt8(n) {
-        this.buffer.writeUInt8(n, this.index++); // Write UInt8 Data & Move index 1 byte
+        this.buffer.writeUInt8(n, this.index++);
         return this;
     }
 
     writeInt16(n, le) {
-        le ? this.buffer.writeInt16LE(n, this.index += 2) : this.buffer.writeInt16BE(n, this.index += 2)); // Write Int16 Data & Move index 2 bytes
+        le ? this.buffer.writeInt16LE(n, this.index) : this.buffer.writeInt16BE(n, this.index);
+        this.index += 2;
         return this;
     }
 
     writeUInt16(n, le) {
-        le ? this.buffer.writeUInt16LE(n, this.index += 2) : this.buffer.writeUInt16BE(n, this.index += 2)); // Write UInt16 Data & Move index 2 bytes
+        le ? this.buffer.writeUInt16LE(n, this.index) : this.buffer.writeUInt16BE(n, this.index);
+        this.index += 2;
         return this;
     }
 
     writeInt32(n, le) {
-        le ? this.buffer.writeInt32LE(n, this.index += 4) : this.buffer.writeInt32BE(n, this.index += 4)); // Write Int32 Data & Move index 4 bytes
+        le ? this.buffer.writeInt32LE(n, this.index) : this.buffer.writeInt32BE(n, this.index);
+        this.index += 4;
         return this;
     }
 
     writeUInt32(n, le) {
-        le ? this.buffer.writeUInt32LE(n, this.index += 4) : this.buffer.writeUInt32BE(n, this.index += 4)); // Write UInt32 Data & Move index 4 bytes
+        le ? this.buffer.writeUInt32LE(n, this.index) : this.buffer.writeUInt32BE(n, this.index);
+        this.index += 4;
         return this;
     }
 
     writeFloat32(n, le) {
-        le ? this.buffer.writeFloatLE(n, this.index += 4) : this.buffer.writeFloatBE(n, this.index += 4)); // Write Float Data & Move index 2 bytes
+        le ? this.buffer.writeFloatLE(n, this.index) : this.buffer.writeFloatBE(n, this.index);
+        this.index += 4;
         return this;
     }
 
     writeString8(n) {
-        if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-        for (var i in n) this.writeUInt8(n.charCodeAt(i)); // Write a UInt8 of each character's char code.
-        return this.writeUInt8(0); // Write a 0 to signify end of string.
+        if (typeof n !== 'string') return console.error('Data must be a string.');
+        for (var i in n) this.writeUInt8(n.charCodeAt(i));
+        return this.writeUInt8(0);
     }
 
     writeString16(n, le) {
-        if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-        for (var i in n) this.writeUInt16(n.charCodeAt(i), le); // Write a UInt16 of each character's char code.
-        return this.writeUInt16(0); // Write a 0 to signify end of string.
+        if (typeof n !== 'string') return console.error('Data must be a string.');
+        for (var i in n) this.writeUInt16(n.charCodeAt(i), le);
+        return this.writeUInt16(0);
     }
 
     writeString32(n, le) {
-        if (typeof n !== 'string') return console.error('Data must be a string.'); // Return error if data passed is not a string
-        for (var i in n) this.writeUInt32(n.charCodeAt(i), le); // Write a UInt32 of each character's char code.
-        return this.writeUInt32(0); // Write a 0 to signify end of string.
+        if (typeof n !== 'string') return console.error('Data must be a string.');
+        for (var i in n) this.writeUInt32(n.charCodeAt(i), le);
+        return this.writeUInt32(0);
     }
 }
 
 class Reader {
     constructor(buffer) {
-        this.index = 0; // Set index to start of buffer
-        this.buffer = buffer; // Set buffer as passed buffer argument
+        this.index = 0
+        this.buffer = buffer;
     }
 
     readInt8() {
-        return this.buffer.readInt8(this.index++); // Return read Int8 data & Move index 1 byte
+        return this.buffer.readInt8(this.index++);
     }
 
     readUInt8() {
-        return this.buffer.readUInt8(this.index++); // Return read UInt8 data & Move index 1 byte
+        return this.buffer.readUInt8(this.index++);
     }
 
     readInt16(le) {
-        this.index += 2; // Move index 2 bytes
-        return le ? this.buffer.readInt16LE(this.index - 2) : this.buffer.readInt16BE(this.index - 2)); // Return read Int16 data
+        this.index += 2;
+        return le ? this.buffer.readInt16LE(this.index - 2) : this.buffer.readInt16BE(this.index - 2);
     }
 
     readUInt16(le) {
-        this.index += 2; // Move index 2 bytes
-        return le ? this.buffer.readUInt16LE(this.index - 2) : this.buffer.readUInt16BE(this.index - 2)); // Return read UInt16 data
+        this.index += 2;
+        return le ? this.buffer.readUInt16LE(this.index - 2) : this.buffer.readUInt16BE(this.index - 2);
     }
 
     readInt32(le) {
-        this.index += 4; // Move index 4 bytes
-        return le ? this.buffer.readInt32LE(this.index - 4) : this.buffer.readInt32BE(this.index - 4)); // Return read Int32 data
+        this.index += 4;
+        return le ? this.buffer.readInt32LE(this.index - 4) : this.buffer.readInt32BE(this.index - 4);
     }
 
     readUInt32(le) {
-        this.index += 4; // Move index 4 bytes
-        return le ? this.buffer.readUInt32LE(this.index - 4) : this.buffer.readUInt32BE(this.index - 4)); // Return read UInt32 data
+        this.index += 4;
+        return le ? this.buffer.readUInt32LE(this.index - 4) : this.buffer.readUInt32BE(this.index - 4);
     }
 
     readFloat32(le) {
-        this.index += 4; // Move index 4 bytes
-        return le ? this.buffer.readFloatLE(this.index - 4) : this.buffer.readFloatBE(this.index - 4)); // Return read Float data
+        this.index += 4;
+        return le ? this.buffer.readFloatLE(this.index - 4) : this.buffer.readFloatBE(this.index - 4);
     }
 
     readString8() {
-        let data = ''; // Set data as a string
-        while (true) { // Run following code until it returns itself
-            let char = this.readUInt8(); // Read char code as UInt8
-            if (char === 0) return data; // Return the string when the character is 0
-            data += String.fromCharCode(char); // Add the character to the data string
+        let data = '';
+        while (true) {
+            let char = this.readUInt8();
+            if (char === 0) return data;
+            data += String.fromCharCode(char);
         }
     }
 
     readString16(le) {
-        let data = ''; // Set data as a string
-        while (true) { // Run following code until it returns itself
-            let char = this.readUInt16(le); // Read char code as UInt16
-            if (char === 0) return data; // Return the string when the char code is 0
-            data += String.fromCharCode(char); // Add the character to the data string
+        let data = ''
+        while (true) {
+            let char = this.readUInt16(le);
+            if (char === 0) return data;
+            data += String.fromCharCode(char);
         }
     }
 
     readString32(le) {
-        let data = ''; // Set data as a string
-        while (true) { // Run following code until it returns itself
-            let char = this.readUInt32(le); // Read char code as UInt32
-            if (char == 0) return data; // Return the string when the char code is 0
-            data += String.fromCharCode(char); // Add the character to the data string
+        let data = '';
+        while (true) {
+            let char = this.readUInt32(le);
+            if (char == 0) return data;
+            data += String.fromCharCode(char);
         }
     }
 }
 
-module.exports = { Writer, Reader }; // Export Writer & Reader
+module.exports = { Writer, Reader };
